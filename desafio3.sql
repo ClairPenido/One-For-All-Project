6e3c08570c707491ae3a190de49138b1b77f702e
Clair Penido <clairpenido@gmail.com>
2022-08-23 13:52:51 -0300

    retirando comentarios para passar no teste da trybe

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,3 @@
--- https://docs.microsoft.com/pt-br/sql/t-sql/functions/round-transact-sql?view=sql-server-ver16 --
--- ROUND(numeric_expression, length) -- 
-
 SELECT u.nome_usuario AS usuario, COUNT(r.id_usuario) AS qt_de_musicas_ouvidas, ROUND(SUM(duracao_musica)/60, 2) AS total_minutos
  FROM
  SpotifyClone.Usuario AS u
